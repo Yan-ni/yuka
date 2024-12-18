d4d07b9ff70f7c507bfa88e1c2af30c23e322161
Anis K <anis01000001@gmail.com>
2024-12-18 16:38:34 +0100

    add cors & morgan

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
 import { connectToDatabase } from './database';
+import morgan from 'morgan';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("combined"));
 
 app.use(routes);
 
